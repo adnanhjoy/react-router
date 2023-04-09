0891788f6969993bed4db7947f475ae2bab42eb4
engadnanhossain <engr.adnanhossain@gmail.com>
2023-04-09 15:28:40 +0600

    added active

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
         <div className='header'>
-            <Link to="/home">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to='/blog'>Blog</Link>
+            <NavLink className={({isActive}) => isActive ? 'active':undefined} to="/home">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to='/blog'>Blog</NavLink>
         </div>
     );
 };
